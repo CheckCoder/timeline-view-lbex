913974c1c809e5b8141d46fbc5abb5b3c05a0e52
checkcai <checkcai@wesure.cn>
2024-01-19 17:35:55 +0800

    feat: 规划组件

---
+++ src/components/BottomInfo.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import { Button } from 'ant-design-vue'
+
+defineProps<{
+  docUrl: string
+  groupUrl: string
+}>()
+
+</script>
+<template>
+  <div class="h-[60px]"></div>
+  <div class="flex flex-row text-sm justify-center items-center w-full fixed bottom-0 h-[60px]">
+    <Button type="link" :href="docUrl" target="blank">使用文档</Button>
+    <span class="text-gray-400">|</span>
+    <Button type="link" :href="groupUrl" target="blank">用户交流群</Button>
+  </div>
+</template>
---
+++ src/views/components/ConfigForm.vue
@@ -0,0 +1,68 @@
+<script setup lang="ts">
+import { Form, FormItem, Button, type FormInstance } from 'ant-design-vue'
+import { FieldType, bitable } from '@lark-base-open/js-sdk';
+import { ref, toRefs } from 'vue';
+import TableSelect from '@/components/TableSelect.vue'
+import FieldSelect from '@/components/FieldSelect.vue';
+import type { Rule } from 'ant-design-vue/es/form';
+
+const formRef = ref<FormInstance>()
+const form = ref<{
+  tableId: string | undefined,
+  startTimeFieldId: string | undefined,
+  endTimeFieldId: string | undefined,
+}>({
+  tableId: undefined,
+  startTimeFieldId: undefined,
+  endTimeFieldId: undefined,
+})
+const formRules: Record<string, Rule[]> = {
+  tableId: [
+    { required: true, message: '请选择表格' },
+  ],
+  startTimeFieldId: [
+    { required: true, message: '请选择开始时间字段' },
+  ],
+  endTimeFieldId: [
+    { required: false, message: '请选择结束时间字段' },
+  ],
+}
+
+const { tableId, endTimeFieldId, startTimeFieldId } = toRefs(form.value)
+bitable.base.getActiveTable().then(async (table) => {
+  const id = table.id
+  tableId.value = id
+  table.getFieldMetaList().then((res) => {
+    console.log(res)
+  })
+})
+
+const loading = ref(false)
+const onSubmit = async () => {
+  await formRef.value?.validate()
+  loading.value = false
+}
+
+</script>
+<template>
+  <Form :layout="'vertical'" :model="form" :rules="formRules" ref="formRef">
+    <FormItem label="表格" name="tableId">
+      <TableSelect v-model:table-id="form.tableId"></TableSelect>
+    </FormItem>
+    <FormItem label="开始时间字段" name="startTimeFieldId">
+      <FieldSelect v-model:table-id="form.tableId" v-model:field-id="form.startTimeFieldId" :field-type-list="[FieldType.DateTime]" placeholder="只支持时间字段">
+      </FieldSelect>
+    </FormItem>
+    <FormItem label="结束时间字段" name="endTimeFieldId">
+      <FieldSelect v-model:table-id="form.tableId" v-model:field-id="form.endTimeFieldId"
+        :field-type-list="[FieldType.DateTime]" placeholder="只支持时间字段" allow-clear></FieldSelect>
+    </FormItem>
+    <FormItem>
+      <div class="flex flex-row items-center">
+        <Button type="primary" @click="onSubmit" :loading="loading">保存</Button>
+        <div class="text-sm text-gray-400 ml-3"></div>
+      </div>
+    </FormItem>
+  </Form>
+</template>
+<style scoped></style>
